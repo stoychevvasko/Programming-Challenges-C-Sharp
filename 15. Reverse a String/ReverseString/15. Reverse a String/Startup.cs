4b8fc734a3a51cba4755147cb012d9a63c45b163
stoychevvasko <stoychev.vassil@gmail.com>
2017-01-23 18:11:44 +0200

    100 C# Challenges - 15. Reverse a String - done
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

---
+++ 15. Reverse a String/ReverseString/15. Reverse a String/Startup.cs
@@ -0,0 +1,25 @@
+﻿////-----------------------------------------------------------------------
+//// <copyright file="Startup.cs" company="independent developer">
+////     Copyright (c) Vassil Stoychev 2017. All rights reserved.
+//// </copyright>
+////-----------------------------------------------------------------------
+namespace StringExtensions
+{
+    /// <summary>
+    /// Program entry.
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Main exe.
+        /// </summary>
+        public static void Main()
+        {
+            string[] strings = { "abracadabra", "abcdefghijklmnopqrstuvwxyz", "0123456789" };
+            foreach (var item in strings)
+            {
+                System.Console.WriteLine("{0} - reversed: {1}", item, StringExtensions.ReverseString(item));
+            }
+        }
+    }
+}
---
+++ 15. Reverse a String/ReverseString/15. Reverse a String/StringExtensions.cs
@@ -0,0 +1,31 @@
+﻿////-----------------------------------------------------------------------
+//// <copyright file="StringExtensions.cs" company="independent developer">
+////     Copyright (c) Vassil Stoychev 2017. All rights reserved.
+//// </copyright>
+////-----------------------------------------------------------------------
+namespace StringExtensions
+{
+    using System.Text;
+
+    /// <summary>
+    /// Provides extended functionality for strings.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Reverses a string.
+        /// </summary>
+        /// <param name="str">A string value.</param>
+        /// <returns>The string in reversed order (backwards).</returns>
+        public static string ReverseString(string str)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < str.Length; i++)
+            {
+                builder.Insert(0, str[i]);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
